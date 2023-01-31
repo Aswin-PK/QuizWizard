b9b7776ecd43ef1ed28802ebfb601399e40ad5b2
Aswin-PK <aswinpk7737@gmail.com>
2023-02-01 01:54:16 +0530

    Added open trivia API for questions

--- game.js
+++ game.js
@@ -3,74 +3,60 @@ const choices = Array.from(document.getElementsByClassName('choice'));
 const solvedandtotal = document.getElementById('solvedandtotal');
 const scores = document.getElementById('scores');
 
+const loading = document.getElementById('loading');
+const game = document.getElementById('innercontainer');
+
+
 console.log(choices);
 let currentQuestion = {};
 let score = 0;
 let questionCounter = 0;
 let availableQuestion = [];
 let acceptingAnswers = true;
-let questions = [
-    {
-        question: "Complete the series 1,6,13,22,33,..",
-        choice1: "46",
-        choice2: "48",
-        choice3: "49",
-        choice4: "51",
-        answer:1
-    },
-    {
-        question: "At a conference, 12 members shook hands with each other before & after the meeting. How many total number of hand shakes occurred ?",
-        choice1: "100",
-        choice2: "122",
-        choice3: "132",
-        choice4: "145",
-        answer:3
-    },
-    {
-        question: "The day after the day after tomorrow is four days before Monday. What day is it today?",
-        choice1: "Monday",
-        choice2: "Tuesday",
-        choice3: "Wednesday",
-        choice4: "Thursday",
-        answer:1
-    },
-    {
-        question: "6, 12, 1, 13, 5 is to flame as 2, 12, 15, 1, 20 is to ?",
-        choice1: "voice",
-        choice2: "bald",
-        choice3: "bloat",
-        choice4: "castle",
-        answer:3
-    },
-    {
-        question: "Moon : Satellite : : Earth : ?",
-        choice1: "Sun",
-        choice2: "Planet",
-        choice3: "Solar System",
-        choice4: "Asteroid",
-        answer:2
-    },
-    {
-        question: "1 : 1 : : 25 : ?",
-        choice1: "26",
-        choice2: "125",
-        choice3: "125",
-        choice4: "625",
-        answer:4
-    }
-]
+let questions = [];
+fetch("https://opentdb.com/api.php?amount=20&category=18&type=multiple")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion =>{
+            const formattedQuestions ={
+                question: loadedQuestion.question
+            };
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestions.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(
+                formattedQuestions.answer - 1,//index
+                0,//how many are removing
+                loadedQuestion.correct_answer//what to be removed
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestions["choice"+(index+1)] = choice;
+            });
+            return formattedQuestions;
+        });
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    })
 
 
 const correctBonus = 5;
 let maxQuestions;
+
 startGame = () =>{
     questionCounter = 0;
     score = 0;
     availableQuestion = [...questions];
-    console.log(availableQuestion);
+    // console.log(availableQuestion);
     maxQuestions = availableQuestion.length;
     // question.innerText = availableQuestion.question;
     getNewQuestions();
+    game.style.display ="flex";
+    loading.style.visibility ="hidden";
 };
 getNewQuestions = () =>{
     if(availableQuestion == 0 || questionCounter >= maxQuestions){
@@ -116,5 +102,3 @@ choices.forEach(choice => {
         }, 1000);
     });
 });
-
-startGame();
